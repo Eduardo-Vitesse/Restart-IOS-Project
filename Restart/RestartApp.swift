44bfd41f832ad3ec6a19ec7bc99be7cde74927a3
Eduardo-Vitesse <edu.dev.py@gmail.com>
2022-05-14 00:54:49 -0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Luiz Eduardo on 14/05/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
